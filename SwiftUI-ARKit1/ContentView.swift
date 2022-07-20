4e59a62e049941234358e29406447de605ef67d6
EthanHalprin <ethan.halprin@gmail.com>
2022-07-20 17:15:00 +0300

    Add resources & materials lists

--- SwiftUI-ARKit1/ContentView.swift
+++ SwiftUI-ARKit1/ContentView.swift
@@ -67,39 +67,6 @@ struct ARViewContainer: UIViewRepresentable {
     func updateUIView(_ uiView: ARView, context: Context) {}
     
 }
-/*
- 
- Materials (iOS 13-14-15)
- 
- /// A basic, unlit material. (Similar to `SimpleMaterial` except that
- ///  this material is not affected by lights in an AR scene.)
- @available(macOS 10.15, iOS 13.0, *)
- public struct UnlitMaterial : Material {
-
- 
- @available(macOS 11.0, iOS 14.0, *)
- public struct VideoMaterial : Material {
-
- 
- @available(macOS 11.0, iOS 15.0, *)
- public struct CustomMaterial : Material {
-
- 
- /// A reference to a function, marked with the [[visible]] attribute, which is contained
- /// within a specified Metal library.
- @available(macOS 11.0, iOS 15.0, *)
- public struct SurfaceShader : MaterialFunction {
-
- 
- /// Creates an invisible material that will hide all objects that are rendered behind the mesh.
- @available(macOS 10.15, iOS 13.0, *)
- public struct OcclusionMaterial : Material {
-
-
- @available(macOS 11.0, iOS 15.0, *)
- public struct PhysicallyBasedMaterial : Material {
- 
- */
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
---
+++ SwiftUI-ARKit1/Resources.swift
@@ -0,0 +1,55 @@
+//
+//  Resources.swift
+//  SwiftUI-ARKit1
+//
+//  Created by Ethan on 20/07/2022.
+//
+/*
+ 
+ ====================================================================================
+ Resources (iOS 13-14-15)
+ ====================================================================================
+
+ /// Defines the material properties (such as static friction, dynamic friction, and restitution) of a physically simulated
+ /// object. See also: `PhysicsBodyComponent`
+ @available(macOS 10.15, iOS 13.0, *)
+ public class PhysicsMaterialResource : Resource {
+
+  
+ /// Represents a shape.
+ ///
+ /// A shape can be shared between multiple colliders.
+ @available(macOS 10.15, iOS 13.0, *)
+ public class ShapeResource : Resource, Hashable {
+
+ 
+ /// Defines a texture resource.
+ @available(macOS 10.15, iOS 13.0, *)
+ public class TextureResource : Resource {
+
+ 
+ /// Represents an animation that can be played using `Entity.playAnimation(...)`.
+ ///
+ /// Animation resources are read-only and can be reused.
+ @available(macOS 10.15, iOS 13.0, *)
+ public class AnimationResource : Resource {
+
+ 
+ /// A playable audio resource
+ @available(macOS 10.15, iOS 13.0, *)
+ public class AudioResource : Resource {
+
+ 
+ /// Defines a environment resource for use in the `ARView.Environment`.
+ @available(macOS 10.15, iOS 13.0, *)
+ public class EnvironmentResource : Resource {
+
+ 
+ /// Represents a mesh resource for rendering.
+ ///
+ /// A `MeshResource` can represent a single triangle mesh or a complex scene of meshes. Meshes can
+ /// be animated, i.e. deformed, at runtime.
+ @available(macOS 10.15, iOS 13.0, *)
+ public class MeshResource : Resource {
+
+ */
