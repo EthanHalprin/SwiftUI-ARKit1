49273806a82c7e3aa3720f34a2ec8db4264e49f3
EthanHalprin <ethan.halprin@gmail.com>
2022-07-20 18:30:57 +0300

    Adding Video Entity Successfully! (along side with Simple)

--- SwiftUI-ARKit1/ContentView.swift
+++ SwiftUI-ARKit1/ContentView.swift
@@ -18,53 +18,102 @@ struct ContentView : View {
 struct ARViewContainer: UIViewRepresentable {
     
     func makeUIView(context: Context) -> ARView {
-        
-        //   V
-        // ARView -> Scene -> Anchors-> Entities Heirachy
+                
+        //
+        // Building AR View is done according to this relations scheme:
+        // https://developer.apple.com/documentation/realitykit/arview
         //
         let arView = ARView(frame: .zero,
                             cameraMode: ARView.CameraMode.ar,
                             automaticallyConfigureSession: true)
-        
-        
+
         //
-        // In order to create a Model Entity of type Mesh:
-        // We need a mesh + material
+        // Add Video Entity
         //
-        //                                      V
-        // ARView -> Scene -> Anchors-> Entities Heirachy
+        if let anchorVideoEntity = createVideoEntity() {
+            anchorVideoEntity.transform.matrix.columns.3.x += 0.1
+            arView.scene.anchors.append(anchorVideoEntity)
+            print("\nCreated & added video entity OK\n")
+        }
+
         //
-        let mesh = MeshResource.generateBox(size: 0.05) // 0.2m
-        let material = SimpleMaterial(color: .systemBlue,
-                                      roughness: 0.5,
-                                      isMetallic: true)
-        let modelEntity = ModelEntity(mesh: mesh, materials: [material])
-        
-        
+        // Add Simple Entity
         //
-        // All model entity has to be attached to an anchor:
-        //
-        //                       V
-        // ARView -> Scene -> Anchors-> Entities Heirachy
-        //
-        let anchorEntity = AnchorEntity()
-        anchorEntity.addChild(modelEntity)
-        
-        
-        //
-        // Need to place anchor in our scene
-        //
-        //             V
-        // ARView -> Scene -> Anchors-> Entities Heirachy
-        //
-        arView.scene.anchors.append(anchorEntity)
-        
-        
+        let anchorSimpleEntity = createSimpleEntity()
+        arView.scene.anchors.append(anchorSimpleEntity)
+        print("\nCreated & added simple entity OK\n")
+
         return arView
 
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+extension ARViewContainer {
+    
+    // Approx 440MB
+    func createSimpleEntity() -> AnchorEntity {
+       
+        //
+        // Reality School
+        // https://www.youtube.com/watch?v=itGRaAryUxA
+        //
+        
+        let mesh = MeshResource.generateBox(size: 0.05)
+        let material = SimpleMaterial(color: .systemBlue,
+                                      roughness: 0.5,
+                                      isMetallic: true)
+        let modelEntity = ModelEntity(mesh: mesh, materials: [material])
+        let anchorEntity = AnchorEntity()
+ 
+        anchorEntity.addChild(modelEntity)
+        
+        return anchorEntity
+    }
+}
+
+extension ARViewContainer {
+    
+    func createVideoEntity() -> AnchorEntity? {
+        
+        //
+        // Apple
+        // https://developer.apple.com/documentation/realitykit/videomaterial
+        //
+        
+        // Create a URL that points to the movie file.
+        guard let url = Bundle.main.url(forResource: "file_example_MP4_480_1_5MG", withExtension: "mp4") else {
+            return nil
+        }
+
+        // Create an AVPlayer instance to control playback of that movie.
+        let player = AVPlayer(url: url)
+
+        //
+        // In order to create a Model Entity of type Mesh:
+        // We need a mesh + material
+        //
+        let cube = MeshResource.generateBox(size: 0.05)
+
+        // Instantiate and configure the video material.
+        let material = VideoMaterial(avPlayer: player)
+
+        // Configure audio playback mode.
+        material.controller.audioInputMode = .spatial
+
+        // Create a new model entity using the video material.
+        let modelEntity = ModelEntity(mesh: cube, materials: [material])
+
+        // Start playing the video.
+        player.play()
+        
+        let anchorEntity = AnchorEntity()
+        anchorEntity.addChild(modelEntity)
+        
+        return anchorEntity
+    }
     
 }
 
